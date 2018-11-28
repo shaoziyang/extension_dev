816d803ec1e2d5e5a88d83dce85789c430ccccde
shaoziyang <shaoziyang@126.com>
2018-11-28 16:29:23 +0800

    BH1750

---
+++ BH1750.ts
@@ -0,0 +1,67 @@
+/**
+ * makecode BH1750 Digital Ambient Light Sensor Package.
+ * From microbit/micropython Chinese community.
+ * http://www.micropython.org.cn
+ */
+
+// BH1750's I2C address
+enum BH1750_ADDRESS {
+    //% block="35"
+    A35,
+    //% block="92"
+    A92
+}
+
+let Address = 35
+
+/**
+ * BH1750 Digital Ambient Light Sensor Package
+ */
+//% weight=100 color=#000011 icon="\uf185"
+namespace BH1750 {
+    /**
+     * set BH1750 Digital Ambient Light Sensor I2C address, default is 35
+     * @param is I2C address, eg: 35
+     */
+    //% blockId="BH1750_SET_ADDRESS" block="set Address %addr"
+    //% weight=100 blockGap=8
+    //% parts=BH1750 trackArgs=0
+    export function SetAddress(addr: BH1750_ADDRESS): void {
+        if (addr == BH1750_ADDRESS.A35)
+            Address = 35
+        else
+            Address = 92
+    }
+
+    /**
+     * turn on BH1750.
+     */
+    //% blockId="BH1750_ON" block="turn on"
+    //% weight=90 blockGap=8
+    //% parts=BH1750 trackArgs=0
+    export function on(): void {
+        pins.i2cWriteNumber(Address, 0x10, NumberFormat.UInt8BE)
+    }
+
+    /**
+     * turn off BH1750, to reduce power consumption.
+     */
+    //% blockId="BH1750_OFF" block="turn off"
+    //% weight=90 blockGap=8
+    //% parts=BH1750 trackArgs=0
+    export function off(): void {
+        pins.i2cWriteNumber(Address, 0, NumberFormat.UInt8BE)
+    }
+
+    /**
+     * get ambient light data (lx)
+     */
+    //% blockId="BH1750_GET_INTENSITY" block="get intensity (lx)"
+    //% weight=80 blockGap=8
+    //% parts=BH1750 trackArgs=0
+    export function getIntensity(): number {
+        return Math.idiv(pins.i2cReadNumber(Address, NumberFormat.UInt16BE) * 5, 6)
+    }
+
+    on();
+}  
---
+++ README.md
@@ -0,0 +1,65 @@
+# BH1750
+
+makecode BH1750 Digital Ambient Light Sensor package for micro:bit  
+
+Author: shaoziyang  
+Date:   2018.Mar  
+  
+![]GY-30.jpg)
+
+## Add extension
+
+open your microbit makecode project, in Add extension, paste  
+
+https://github.com/microbit-makecode-packages/BH1750  
+
+to search box then search.
+
+## Basic usage
+```
+let Intensity = 0
+BH1750.SetAddress(BH1750_ADDRESS.A35)
+led.plotBarGraph(
+100,
+100
+)
+basic.forever(() => {
+    Intensity = BH1750.getIntensity()
+    led.setBrightness(20 + Intensity / 512)
+    basic.pause(500)
+}) 
+```
+
+## I2C Address  
+- 35/92 
+
+## API
+
+- SetAddress(addr: BH1750_ADDRESS)  
+addr is BH1750 address, it maybe 35 or 92  
+
+- on()  
+turn on BH1750.
+
+- off()
+turn off BH1750, to reduce power consumption.
+
+- getIntensity()  
+get ambient light data
+
+## Demo
+
+![](demo.jpg)
+
+## License
+
+MIT
+
+Copyright (c) 2018, microbit/micropython Chinese community  
+
+## Supported targets
+
+* for PXT/microbit
+
+
+[From microbit/micropython Chinese community](http://www.micropython.org.cn)
